888e61b6634740c4453797459e958bef526b4215
Huynh The Khoa <lilo190516@gmail.com>
2023-09-27 10:53:54 +0700

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express()
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 const postRoutes = require('./api/routes/posts')
+const cors = require('cors')
+
+app.use(cors({
+    origin: "*"
+}))
 
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
